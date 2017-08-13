7e472e91e6bddb1216d0aea55d4810be26d0c752
Denis Buzmakov <lysva94@mail.ru>
2017-08-13 22:45:50 +0300

    added some tests for add city fragment

--- app/src/androidTest/java/com/zino/mobilization/weatheryamblz/AddCityFragmentTest.java
+++ app/src/androidTest/java/com/zino/mobilization/weatheryamblz/AddCityFragmentTest.java
@@ -12,11 +12,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.core.IsNot.not;
 
 /**
@@ -41,11 +43,29 @@ public class AddCityFragmentTest {
     }
 
     @Test
+    public void shouldShowClearButton() {
+        onView(withId(R.id.btn_clear)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.txt_input)).perform(typeText("k"));
+        onView(withId(R.id.btn_clear)).check(matches(isDisplayed()));
+        onView(withId(R.id.txt_input)).perform(clearText());
+        onView(withId(R.id.btn_clear)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.txt_input)).perform(typeText("kk"));
+        onView(withId(R.id.btn_clear)).check(matches(isDisplayed()));
+    }
+
+    @Test
     public void shouldHideLoadingAndShowList() {
         onView(withId(R.id.txt_input)).perform(typeText("k"));
         onView(withId(R.id.progress)).check(matches(isDisplayed()));
         SystemClock.sleep(1000);
         onView(withId(R.id.progress)).check(matches(not(isDisplayed())));
         onView(withId(R.id.list_suggestions)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void shouldClearInput() {
+        onView(withId(R.id.txt_input)).perform(typeText("k"));
+        onView(withId(R.id.btn_clear)).perform(click());
+        onView(withId(R.id.txt_input)).check(matches(withText("")));
     }
 }
